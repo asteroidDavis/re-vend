58662cacdd3ea9f82591c8d31f8f24d3d313ba04
asteroidDavis <natedavis174@gmail.com>
2015-08-09 12:15:57 -0400

    I added a class to control a locking solenoid.

---
+++ solenoid.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+
+#sets up a solenoid object
+#parameters:
+#   lockPin: The GPIO pin controlling the lock
+#   unlockTime: The delay between unlocking and locking in seconds
+def __init__(self, lockPin, unlockTime):
+    GPIO.setmode(GPIO.BCM)
+
+    self.lockPin = lockPin
+    self.unlockTime = unlockTime
+
+#extends the solenoid by removing power
+def lock(self):
+    GPIO.output(self.lockPin, 0)
+
+#unlocks the solenoid by running current throug it
+def unlock(self):
+    GPIO.output(self.lockPin, 1)
+
+#unlocks for the unlockTime
+def unlockThenLock(self):
+    self.lock()
+    time.sleep(self.unlockTime)
+    self.unlock()
+
